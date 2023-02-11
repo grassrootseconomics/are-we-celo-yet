07e87cf6ff61913a3ec3b864241ccbbbada4eaf8
lash <dev@holbrook.no>
2023-02-11 12:47:42 +0000

    Use apply-block for block

--- chainlib_eth_celo/__init__.py
+++ chainlib_eth_celo/__init__.py
@@ -1,2 +1,2 @@
-from .block_dialect_filter import CeloBlockDialectFilter
+from .block_dialect_filter import *
 from .util import CeloUtil
--- chainlib_eth_celo/block_dialect_filter.py
+++ chainlib_eth_celo/block_dialect_filter.py
@@ -1,7 +1,5 @@
 from chainlib.dialect import DialectFilter as BaseDialectFilter
 
-class CeloBlockDialectFilter(BaseDialectFilter):
-    def apply_src(self, src):
-        src['gas_limit'] = '0x0'
-        return src
-    
+class DialectFilter(BaseDialectFilter):
+    def apply_block(self, block):
+        block.src['gas_limit'] = '0x0'
